34476510fcbc63d5e0ac9f14df8e276d9ebd2b0c
Jack Kelly <Carnificis@users.noreply.github.com>
2023-02-07 13:23:10 -0600

    weather api

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,24 @@
 var parksKey = "KVZPIz8u4dvHQhj5wYQxDQUisHC1pnhaaCNJSTD9";
-var parkCode = "acad" // placeholder (pulls data for Acadia)
-var parksUrl = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkCode + "&api_key=" + parksKey;
+var parkCode = "acad"; // placeholder (pulls data for Acadia)
+var parksUrl =
+  "https://developer.nps.gov/api/v1/parks?parkCode=" +
+  parkCode +
+  "&api_key=" +
+  parksKey;
+var weatherUrl = "https://api.weather.gov/points/";
 
-fetch(parksUrl)
-    .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-            });
-        }
+fetch(parksUrl).then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      console.log(data);
     });
+  }
+});
+
+fetch(weatherUrl).then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  }
+});
